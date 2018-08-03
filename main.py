12cfe4a14aadcc2bc838717b0d62c4ed4862523b
Cho Hye Min <pedalioaspare@gmail.com>
2018-08-03 10:17:34 -0700

    Config update

--- main.py
+++ main.py
@@ -277,7 +277,9 @@ def evaluate():
     # imid = 64  # 0: 企鹅  81: 蝴蝶 53: 鸟  64: 古堡
     imid = 0  # 0: 企鹅  81: 蝴蝶 53: 鸟  64: 古堡
     valid_lr_img = valid_lr_imgs[imid]
-    valid_lr_img = tl.prepro.imresize(valid_lr_img, size=[1024, 1403, 3]);
+
+    valid_lr_img = np.concat((vlid_lr_img, valid_lr_img, valid_lr_img), axis=2)
+    # valid_lr_img = tl.prepro.imresize(valid_lr_img, size=[1024, 1403, 3]);
     print("resized: " + str(valid_lr_img.shape))
     # valid_hr_img = valid_hr_imgs[imid]
     # valid_lr_img = get_imgs_fn('test.png', 'data2017/')  # if you want to test your own image
