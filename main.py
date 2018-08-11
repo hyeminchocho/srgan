13a0eceb547bc14781548b0ab81e40509e9cc8cd
Hye Min Cho <3820319+hyeminchocho@users.noreply.github.com>
2018-08-11 15:06:05 -0700

    Frames 01 and rudimentary batch evaluate

--- main.py
+++ main.py
@@ -343,6 +343,7 @@ def evaluate():
                 curr_valid_lr_img = valid_lr_img[i]
                 curr_valid_lr_img = (curr_valid_lr_img / 127.5) - 1  # rescale to ［－1, 1]
                 curr_valid_lr_img = np.expand_dims(curr_valid_lr_img, axis=0)
+                print("curr valid img shape: " + str(curr_valid_lr_img.shape))
                 res_img = np.concatenate((res_img, curr_valid_lr_img), axis=0)
                 
             # valid_lr_img = valid_lr_imgs[imid:imid+config.TRAIN.batch_size]
